14a869513af07f03784ccf0b14eb7a884a4809e9
mohammadariful20 <arifulz848@gmail.com>
2024-04-07 23:29:34 +0600

    theme change setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  themes: ["light", "synthwave"],
   plugins: [require("daisyui")],
 }
 
