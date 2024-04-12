61c6758c1da46bea5e6d99fc2b07650123015ad1
mohammadariful20 <arifulz848@gmail.com>
2024-04-13 01:49:04 +0600

    sort function done

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import ComminBanner from "../components/ComminBanner";
 import { useLoaderData } from 'react-router-dom'
 
 
 export default function Programs() {
-  const programs = useLoaderData()
-  console.log(programs)
+  const programs = useLoaderData();
+  const [program,setProgram]=useState([])
+
+  useEffect(() => {
+    // This function will be called when the component is mounted
+    handleSort();
+  }, []);
+
+  const handleSort = (sort) => {
+    setProgram(programs)
+    if (sort) {
+      let soryBy=programs.filter((sItem)=>sItem.category===sort)
+      setProgram(soryBy)
+    }
+    if(sort==='all'){
+      setProgram(programs)
+    }
+  }
+  
   return (
     <div>
       <ComminBanner location={'Programs'}></ComminBanner>
@@ -12,14 +30,14 @@ export default function Programs() {
       <p className="text-3xl italic mb-10 font-sotify text-[#162C5A] text-center">Make a difference life of a <br />child with special needs</p>
 
       <div className="justify-center flex w-full gap-4 my-20">
-        <button className="btn btn-warning rounded-3xl">All Programs</button>
-        <button className="btn btn-warning rounded-3xl">Family</button>
-        <button className="btn btn-warning rounded-3xl">Volunteers</button>
-        <button className="btn btn-warning rounded-3xl">Community</button>
+        <button onClick={()=>handleSort('all')} className="btn btn-warning rounded-3xl">All Programs</button>
+        <button onClick={()=>handleSort('Family')} className="btn btn-warning rounded-3xl">Family</button>
+        <button onClick={()=>handleSort('Volunteers')} className="btn btn-warning rounded-3xl">Volunteers</button>
+        <button onClick={()=>handleSort('Community')} className="btn btn-warning rounded-3xl">Community</button>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 my-10">
         {
-          programs.map((item) => {
+          program.map((item) => {
             return (
               <div key={item.id} className="relative mx-auto hover:rotate-[360deg] duration-1000">
                 <img src={item.image} className="w-96 h-96 rounded-full opacity-85" alt={item.title} />
